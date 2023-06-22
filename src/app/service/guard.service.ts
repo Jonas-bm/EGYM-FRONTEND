dfc9f05dc5fcc5ea23ca0b13780bf530dc08cc4b
Dariohash <105894662+Dariohash@users.noreply.github.com>
2023-06-21 19:16:11 -0500

    D

--- src/app/service/guard.service.ts
+++ src/app/service/guard.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { LoginService } from './login.service';
-import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
